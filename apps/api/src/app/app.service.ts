ceecda2d85e867471ef5ae1c7231fe1a58d1f4d3
Rod Pattison <rodpattison@gmail.com>
2021-11-02 05:58:05 +0800

    Testing CD

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Message } from '@nx-stack/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to api!' };
+    return { message: 'Welcome to api updated!' };
   }
 }
